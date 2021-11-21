ff33b880cca5e17de2c7d50d4c513a6a314aa20c
dianeshan <dianeshan40785@gmail.com>
2021-11-21 11:08:09 -0800

    Add comments

--- proc.c
+++ proc.c
@@ -200,7 +200,7 @@ fork(void)
   np->sz = curproc->sz;
   np->parent = curproc;
   *np->tf = *curproc->tf;
-  np->pages = curproc->pages;
+  np->pages = curproc->pages; //lab3
 
   // Clear %eax so that fork returns 0 in the child.
   np->tf->eax = 0;
